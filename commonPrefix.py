8d010a05f75f634985a49144e17ef31fe4243dd1
pykira-cpu <sainagaakhil@gmail.com>
2023-01-25 18:06:11 -0600

    longest Common Prefix

---
+++ commonPrefix.py
@@ -0,0 +1,43 @@
+# Print largest common prefix in a list of strings
+
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        def commonPrefixUtil(str1, str2):
+            n1 = len(str1)
+            n2 = len(str2)
+            result = ""
+            i = 0
+            j = 0
+
+            while i < n1 and j < n2:
+                if (str1[i]!= str2[j]):
+                    break
+                result += str1[i]
+                i += 1
+                j += 1
+            return result
+
+        
+        prefix = strs[0]
+        
+        for i in range(1,len(strs)):
+            prefix = commonPrefixUtil(prefix, strs[i])
+        
+        return prefix
+
+
+#Test Cases:
+
+print(commonPrefixUtil("flower", "flow"))
+
+print(commonPrefixUtil("ab", "a"))
+
+print(commonPrefixUtil("a", ""))
+
+
+
